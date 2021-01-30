ef95c76c96c5267b8c2cca832fccc39900c53668
Mollycodd1e <selik7@mail.ru>
2021-01-30 19:56:05 +0300

    Первая страницы адаптивная

--- source/js/faq.js
+++ source/js/faq.js
@@ -27,25 +27,3 @@
     openToggle();
   }
 })();
-
-'use strict';
-
-(function () {
-  var navMain = document.querySelector('.main-nav');
-
-  if (navMain) {
-    var navToggle = document.querySelector('.main-nav__toggle');
-
-    navMain.classList.add('main-nav--closed');
-
-    navToggle.addEventListener('click', function () {
-      if (navMain.classList.contains('main-nav--closed')) {
-        navMain.classList.add('main-nav--opened');
-        navMain.classList.remove('main-nav--closed');
-      } else {
-        navMain.classList.remove('main-nav--opened');
-        navMain.classList.add('main-nav--closed');
-      }
-    });
-  }
-})();
