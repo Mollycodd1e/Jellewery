74436b8ab3938f9c9d7dea8744b64fce789763e7
Mollycodd1e <selik7@mail.ru>
2021-01-31 08:10:02 +0300

    Адаптивная версия десктопа каталога

--- source/js/faq.js
+++ source/js/faq.js
@@ -4,26 +4,27 @@
   var mainFaq = document.querySelector('.main-faq');
 
   if (mainFaq) {
-    var mainWrapper = document.querySelector('.main-faq__wrapper');
-    var materialFaq = mainFaq.querySelector('.main-faq__material-wrapper');
-    var deliveryFaq = mainFaq.querySelector('.main-faq__delivery-wrapper');
-    var paymentFaq = mainFaq.querySelector('.main-faq__payment-wrapper');
-    var returnFaq = mainFaq.querySelector('.main-faq__return-wrapper');
-    var allFaq = mainWrapper.querySelectorAll('div');
-    var allButtons = allFaq.querySelectorAll('button');
-    console.log(allFaq);
+    var allFaq = mainFaq.querySelectorAll('.main-faq__faq-wrapper');
 
-    var openToggle = function () {
-      for (var i = 0; i < allFaq.length; i ++) {
-        allButtons[i].addEventListener('click', function () {
-          if (allFaq[i].classList.contains('main-faq__' + wrapper + '--close')) {
-            allFaq[i].classList.remove('main-footer__' + wrapper + '--close');
-            allFaq[i].classList.add('main-footer__' + wrapper + '--open');
-          }
-        });
-      }
+    var openToggle = function (question) {
+      question.addEventListener('click', function () {
+        var openTab = mainFaq.querySelector('.main-faq__faq-wrapper--open');
+
+        if (question.classList.contains('main-faq__faq-wrapper--close')) {
+          question.classList.remove('main-faq__faq-wrapper--close');
+          question.classList.add('main-faq__faq-wrapper--open');
+        } else {
+          question.classList.remove('main-faq__faq-wrapper--open');
+          question.classList.add('main-faq__faq-wrapper--close');
+        }
+
+        openTab.classList.remove('main-faq__faq-wrapper--open');
+        openTab.classList.add('main-faq__faq-wrapper--close');
+      });
+    };
+
+    for (var i = 0; i < allFaq.length; i++) {
+      openToggle(allFaq[i]);
     }
-
-    openToggle();
   }
 })();
