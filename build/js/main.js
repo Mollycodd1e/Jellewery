193d2293603c0d1d74d32e73d51c6d88f3f6ed3a
Mollycodd1e <selik7@mail.ru>
2021-02-02 05:56:50 +0300

    Неполная мобильная версия каталога

--- build/js/main.js
+++ build/js/main.js
@@ -75,16 +75,20 @@
 
   if (navMain) {
     var navToggle = document.querySelector('.main-nav__toggle');
+    var header = document.querySelector('.main-header');
 
+    header.classList.remove('main-header--opened');
     navMain.classList.add('main-nav--closed');
 
     navToggle.addEventListener('click', function () {
       if (navMain.classList.contains('main-nav--closed')) {
         navMain.classList.add('main-nav--opened');
         navMain.classList.remove('main-nav--closed');
+        header.classList.add('main-header--opened');
       } else {
         navMain.classList.remove('main-nav--opened');
         navMain.classList.add('main-nav--closed');
+        header.classList.remove('main-header--opened');
       }
     });
   }
