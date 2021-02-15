79cda00d7059da3c890a237062f0c7370d675f45
Mollycodd1e <selik7@mail.ru>
2021-02-15 16:13:35 +0300

    второе исправление после тестирования

--- source/js/slider.js
+++ source/js/slider.js
@@ -4,18 +4,36 @@
   var slider = document.querySelector('.main-new');
 
   if (slider) {
-    var previousSlide = slider.querySelector('.main-new__back-button-wrapper button');
-    var nextSlide = slider.querySelector('.main-new__forward-button-wrapper button');
+    var position = 0;
+    var slidesToShow = 4;
+    var slidesToScroll = 2;
+    var previousButton = slider.querySelector('.main-new__back-button-wrapper button');
+    var nextButton = slider.querySelector('.main-new__forward-button-wrapper button');
     var sliderList = slider.querySelector('.main-new__product-list');
+    var sliderContainer = slider.querySelector('.main-new__product-list-wrapper');
+    var sliderItems = slider.querySelectorAll('.main-new__product-item');
+    var itemWidth = sliderContainer.clientWidth / slidesToShow;
+    var widthNow = sliderList.offsetWidth;
+    var movePosition = slidesToScroll * itemWidth;
 
-    previousSlide.addEventListener('click', function () {
-      var widthNow = sliderList.offsetWidth;
-      sliderList.style.transform = 'translateX(-' + widthNow + 'px)';
+    if (window.innerWidth < 1024) {
+      slidesToShow = 2;
+    } else {
+      slidesToShow = 4;
+    }
+
+    //sliderItems.forEach(function(item) {
+    //  item.style.minWidth = ' ' + itemWidth  + 'px';
+    //});
+
+    previousButton.addEventListener('click', function () {
+      position += widthNow;
+      sliderList.style.transform = 'translateX(-' + position + 'px)';
     });
 
-    nextSlide.addEventListener('click', function () {
-      var widthNow = sliderList.offsetWidth;
-      sliderList.style.transform = 'translateX(' + widthNow + 'px)';
+    nextButton.addEventListener('click', function () {
+      position -= widthNow;
+      sliderList.style.transform = 'translateX(-' + position + 'px)';
     });
   }
 })();
